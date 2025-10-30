42aa85e556ebedf5bbe91069854c3f3b80d65cfd
IGOR SAIONETI AMORIM <igor.samorim1@senacsp.edu.br>
2025-10-30 17:05:32 -0300

    aula 30-10

---
+++ matematica/matematica/Program.cs
@@ -0,0 +1,46 @@
+﻿//Declarando constantes 
+
+const int valor1 = 10;
+const int valor2 = 5;
+
+//Soma
+int soma = valor1 + valor2;
+Console.WriteLine($"Soma entre os dois valores: " + soma);
+
+//Subtração
+int subtracao = valor1 - valor2;
+Console.WriteLine($"Subtração entre os dois valores: " + subtracao);
+
+//Multiplicação
+int multiplicacao = valor1 * valor2;
+Console.WriteLine($"Multiplicação entre os dois valores: " + multiplicacao);
+
+//Divisão
+int divisao = valor1 / valor2;
+Console.WriteLine($"Divisão entre os dois valores: " + divisao);
+
+//Resto da divisão
+int restDivisao = valor1 % valor2;
+Console.WriteLine($"Resto da divisão entre os dois valores: " +  restDivisao);
+
+//Potenciação
+double potencia = Math.Pow(valor1, valor2);
+Console.WriteLine($"Potenciação entre os valores: {valor1} elevado a {valor2} = {potencia} ");
+
+int resultado1 = 3 + 5 * 2;
+Console.WriteLine($"Resultado 1 (sem parênteses): {resultado1}");
+
+int resultado2 = (3 + 5) * 2;
+Console.WriteLine($"Resultado 2 (com parênteses): {resultado2}");
+
+int resultado3 = 8 / 4 + 3;
+Console.WriteLine($"Resultado 3 (sem parênteses): {resultado3}");
+
+double resultado4 = 8.0 / (4 + 3);
+Console.WriteLine($"Resultado 4 (com parênteses): {resultado4}");
+
+
+
+
+
+
---
+++ variaveisecostantes/variaveisecostantes/Program.cs
@@ -0,0 +1,52 @@
+﻿// Variaveis
+char letra = 'A';
+string nome = "João";
+float temperatura = 36.5f;
+double salario = 1234.67;
+bool estaChovendo = true;
+
+
+// Alterando as variáveis
+letra = 'B';
+nome = "Maria";
+temperatura = 37.0f;
+salario = 15000.50;
+estaChovendo = false;
+
+const char simbolo = '$';
+const string saudacao = "Bem-vindo ao C#";
+const float pi = 3.14159f;
+const double altura = 1.75;
+const bool verdade = false;
+const int numero = 14;
+
+
+//Exibindo valores na tela
+Console.WriteLine("Valores originais: ");
+Console.WriteLine($"Letra: {letra}");
+Console.WriteLine($"Nome: {nome}");
+Console.WriteLine($"Temperatura: {temperatura}");
+Console.WriteLine($"Slário: {salario}");
+Console.WriteLine($"Está chovendo? {estaChovendo}");
+
+//Exibindo valores originais por concatenação
+Console.WriteLine("\nConstantes:");
+Console.WriteLine("Simbolo: " + simbolo);
+Console.WriteLine("Saudação: " + saudacao);
+Console.WriteLine("Valor de pi: " + pi);
+Console.WriteLine("Altura: " + altura);
+Console.WriteLine("È verdade? " + verdade);
+Console.WriteLine("Número: " + numero); 
+
+
+
+
+
+
+
+
+
+
+
+
+
