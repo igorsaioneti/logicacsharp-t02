9f8355b4f4a1bde5cd09f43e86acef68afb05b9c
IGOR SAIONETI AMORIM <igor.samorim1@senacsp.edu.br>
2025-11-06 14:58:23 -0300

    enviando atualização

---
+++ operadoreslogicos/operadoreslogicos/Program.cs
@@ -0,0 +1,12 @@
+﻿bool x = true;
+bool y = false; 
+
+bool and = x && y;  
+bool or = x ||  y;
+bool not = !x;
+
+Console.WriteLine("\nOperadores Lógicos:"); 
+Console.WriteLine($"x e y: {and}"); // E lógico
+Console.WriteLine($"x ou y: {or}"); // Ou lógico
+Console.WriteLine($"X negado: {not}"); // Não lógico
+
